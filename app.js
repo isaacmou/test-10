de80fb754e02de820af51c661350f3d6ffa8eb3c
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-25 01:35:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ function showEnglish() {
     document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
     document.getElementById("engInput").style.display = "";
     document.getElementById("chiInput").style.display = "none";
+    document.getElementById("Engdistrictselect").style.display = "";
+    document.getElementById("EnglishDistrictfuncButton").style.display = "";
+    document.getElementById("Chidistrictselect").style.display = "none";
+    document.getElementById("ChineseDistrictfuncButton").style.display = "none";
     document.getElementById("engVerwithdistrict").style.display = "none";
     
     
@@ -15,6 +19,10 @@ function showChinese() {
     document.getElementById("toptitle").innerHTML = "健步行徑";
     document.getElementById("engInput").style.display = "";
     document.getElementById("chiInput").style.display = "none";
+    document.getElementById("Engdistrictselect").style.display = "none";
+    document.getElementById("EnglishDistrictfuncButton").style.display = "none";
+    document.getElementById("Chidistrictselect").style.display = "";
+    document.getElementById("ChineseDistrictfuncButton").style.display = "";
     document.getElementById("engVerwithdistrict").style.display = "none";
   
    
